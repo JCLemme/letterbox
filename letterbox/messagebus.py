c8de67a019b235e29c4b2bcd4f68c7649e7cd028
John C Lemme <jclemme@proportionallabs.com>
2019-12-12 14:44:39 -0500

    Initial upload

--- letterbox/messagebus.py
+++ letterbox/messagebus.py
@@ -15,12 +15,33 @@ import zmq
 # written by John Lemme, 2019-2020 (jclemme at my dot uri dot edu)
 
 
+class Bus:
+    server = ""
+    ctx = None
+    sok_send = None
+    sok_recv = None
+    recv_hdlr = None
+    
+    def send(message:Message):
+        socket.send(message.topic, zmq.SENDMORE)
+        socket.send_pyobj(message.messagedata)
+        
+    def recv_subscriptions_set(subs):
+        for sub in subs:
+            self.sok_recv.setsockopt(zmq.SUBSCRIBE, bytes(sub))
+        
+    def recv_handler_set(handler):
+        self.recv_hdlr = handler
+        
+    def recv_run():
+    
 class Message:
     type = b""
     data = {}
     
-def send(message:Message):
-def  
+def connect(server):
+
+
 
 def main_server():
 
